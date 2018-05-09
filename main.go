781acb73f3a5a62437850409b2573c4550bc8c9e
bannzai <kingkong999yhirose@gmail.com>
2018-05-09 11:08:41 +0900

    :metal: WIP read file and filter by optoin

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"os/exec"
+	"strings"
 )
 
 func contains(array []string, s string) bool {
@@ -18,42 +18,72 @@ func contains(array []string, s string) bool {
 }
 
 func parseFileContent(content []byte) []string {
+	return split(string(content), "\n")
+}
 
+func split(content string, separator string) []string {
+	return strings.Split(content, separator)
 }
 
 func convertToFileNames(argument string) []string {
 	data, _ := ioutil.ReadFile(argument)
 	if data != nil {
-		return
+		return parseFileContent(data)
 	}
-
+	return split(argument, ",")
 }
 
 func main() {
 	var (
-		fs     = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-		ignore = fs.String("ignore", "", "Do not output file-list")
-		only   = fs.String("only", "", "Show only file-list.")
+		ignore string
+		only   string
 	)
 
+	fs := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	fs.StringVar(&ignore, "ignore", "", "Do not output file-list")
+	fs.StringVar(&only, "only", "", "Show only file-list.")
 	fs.Parse(os.Args[2:])
+
 	fmt.Printf("os.Args() = %+v\n", os.Args)
+	fmt.Printf("ignore = %+v\n", ignore)
+	fmt.Printf("only = %+v\n", only)
 
 	if len(os.Args) < 2 {
 		panic("Should specifity directory. e.g file-list /path/to/directory")
 	}
 
-	fmt.Printf("ignore = %+v\n", *ignore)
-	fmt.Printf("only = %+v\n", *only)
-
-	// directory := os.Args[0]
-
-	cmd := exec.Command("find", "./", "-maxdepth", "1", "-type", "f", "-not", "-name", "'user.go'")
-	fmt.Printf("cmd = %+v\n", cmd)
-	output, err := cmd.Output()
+	directory, err := ioutil.ReadDir(os.Args[1])
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("Can not read directory: %v", os.Args[1]))
 	}
 
-	fmt.Printf("output = %+v\n", string(output))
+	ignoreFiles := convertToFileNames(ignore)
+	onlyFiles := convertToFileNames(only)
+	hasIgnore := len(ignore) > 0
+	hasOnly := len(only) > 0
+
+	fmt.Printf("ignoreFiles = %+v\n", ignoreFiles)
+	fmt.Printf("hasIgnore = %+v\n", hasIgnore)
+	fmt.Printf("hasOnly = %+v\n", hasOnly)
+	if hasIgnore && hasOnly {
+		panic("Can not be shared option. --ignore and --only")
+	}
+
+	var files []string
+	for _, f := range directory {
+		fileName := f.Name()
+
+		if contains(ignoreFiles, fileName) {
+			continue
+		}
+
+		if hasOnly && contains(onlyFiles, fileName) {
+			files = append(files, fileName)
+			continue
+		}
+
+		files = append(files, fileName)
+	}
+
+	fmt.Printf("files = %+v\n", files)
 }
